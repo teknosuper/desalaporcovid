481b90fe4465971ae43309fcb836f846ec112be0
teknosuper <ponyow@gmail.com>
2020-04-01 06:10:04 +0700

    gitignore

--- config/db-local.php
+++ config/db-local.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=desalaporcovid',
     'username' => 'root',
-    'password' => '',
+    'password' => 'teknowdm05',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
