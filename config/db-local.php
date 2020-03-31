6e60dd329f9ce148b5dc11fa23fcf95a741dd966
teknosuper <ponyow@gmail.com>
2020-04-01 06:09:06 +0700

    commit

--- config/db-local.php
+++ config/db-local.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=desalaporcovid',
     'username' => 'root',
-    'password' => 'teknowdm05',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
